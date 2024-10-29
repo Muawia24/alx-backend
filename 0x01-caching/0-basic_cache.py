5f7b8c2ab32c3920ad2f851f9d2f593eabe52b8d
Ahmed Muawia <amuawia666@gmail.com>
2024-10-29 15:15:43 +0300

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+0. Basic dictionary
+"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        """
+        Assign to the dictionary self.cache_data the item
+        value for the key key.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return the value in self.cache_data linked to key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data.get(key)
