13e2423779fef914b5a5fab618ef386f29e99248
Ahmed Muawia <amuawia666@gmail.com>
2024-11-05 18:54:43 +0200

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+1. Basic Babel setup
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.config.from_object('config.Config')
+
+babel = Babel(app)
+
+
+@app.route("/")
+def hello_holberton():
+    """
+    Welcome to Holberton
+    """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
